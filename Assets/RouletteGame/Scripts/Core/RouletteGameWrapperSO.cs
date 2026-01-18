fac6cc10619436421456afaae0eecddba4b9a15c
UmutCanT <uctekcambusiness@gmail.com>
2026-01-18 16:35:35 +0300

    created: Roullete Game Data Wrapper

---
+++ Assets/RouletteGame/Scripts/Core/RouletteGameWrapperSO.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RouletteGame.Core
+{
+    [CreateAssetMenu(fileName = "RouletteGameWrapperSO", menuName = "Roulette/Roullete Game Spin Wrapper")]
+    public class RouletteGameWrapperSO : ScriptableObject
+    {
+        [SerializeField] private RouletteDataSO[] rouletteData;
+    }
+}
