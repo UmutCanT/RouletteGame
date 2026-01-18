bbf11e425852b8ab557cdeaff142006bbac41a62
UmutCanT <uctekcambusiness@gmail.com>
2026-01-18 16:09:02 +0300

    created: Reward Roulette SO Data

--- Assets/RouletteGame/Scripts/Core/RouletteDataSO.cs
+++ Assets/RouletteGame/Scripts/Core/RouletteDataSO.cs
@@ -10,6 +10,6 @@ namespace RouletteGame.Core
         [SerializeField] private Sprite rouletteSprite;
         [SerializeField] private Sprite rouletteIndicator;
         [SerializeField] private RouletteType rouletteType;
-        [SerializeField] private List<RouletteElementDataSO> rouletteElement;
+        [SerializeField] private RouletteElementDataSO[] rouletteElement;
     }
 }
