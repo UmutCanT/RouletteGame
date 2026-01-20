afaa5f369e5f3e4cce298b565eca8df0cb49a4fc
UmutCanT <uctekcambusiness@gmail.com>
2026-01-20 17:08:41 +0300

    update: Reward Types

--- Assets/RouletteGame/Scripts/Core/RewardType.cs
+++ Assets/RouletteGame/Scripts/Core/RewardType.cs
@@ -6,6 +6,18 @@ namespace RouletteGame.Core
 {
     public enum RewardType
     {
-
+        Cash,
+        GoldCoin,
+        Consumables,
+        Throwables,
+        ArmorPoints,
+        KnifePoints,
+        PistolPoints,
+        RiflePoints,
+        ShotgunPoints,
+        SubmachinePoints,
+        SniperPoints,
+        Chests,
+        Skins
     }
 }
--- Assets/RouletteGame/Scripts/UI/CurrentRewardElementUI.cs
+++ Assets/RouletteGame/Scripts/UI/CurrentRewardElementUI.cs
@@ -1,3 +1,4 @@
+using RouletteGame.Core;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -8,9 +9,10 @@ namespace RouletteGame.UI
 {
     public class CurrentRewardElementUI : MonoBehaviour
     {
-        [SerializeField] private string rewardType;
-        [SerializeField] private Image rewardElementIcon;
+        [SerializeField] private RewardType rewardType;
         [SerializeField] private TextMeshProUGUI rewardElementAmountText;
+
+        public RewardType RewardType => rewardType;
 
         public void UpdateRewardElementText(int newAmount)
         {
